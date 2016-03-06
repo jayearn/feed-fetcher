0610b3c2de41111b374b7c4d98ac016f1ed87605
Jörn Ahrens <joern.ahrens@gmail.com>
2016-03-06 13:27:29 +0100

    shell script wrapper and parameterized js

---
+++ fetch-feed.js
@@ -0,0 +1,37 @@
+var FeedParser = require('feedparser')
+  , request = require('request');
+
+var req = request(process.argv[2])
+  , feedparser = new FeedParser();
+
+req.on('error', function (error) {
+  // handle any request errors
+});
+req.on('response', function (res) {
+  var stream = this;
+
+  if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+
+  stream.pipe(feedparser);
+});
+
+
+feedparser.on('error', function(error) {
+  // always handle errors
+});
+feedparser.on('readable', function() {
+  // This is where the action is!
+  var stream = this
+    , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+    , item;
+
+  while (item = stream.read()) {
+    console.log('Title: ' + item.title);
+    console.log('Link: ' + item.link);
+//    console.log('Summary: ' + item.summary);
+    console.log('GUID: ' + item.guid);
+    console.log('date: ' + item.date);
+    console.log('pubdate: ' + item.pubdate);
+    console.log();
+  }
+});
---
+++ fetch-feeds.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for FILE in `cat feedlist` ;
+do
+    node fetch-feed.js $FILE
+done
+
+exit 0
